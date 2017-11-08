7a7effb52dd93d46dc5ddaf672675bf8b150e706
Brian Chotisunanta <bc.fsdev@gmail.com>
2017-11-07 23:57:56 -0800

    adding another restaurant to the restaurantList array

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -32,7 +32,9 @@ restaurantList.push(new Restaurant(restaurantId++, "Olive Pit Grill", "2445 E. I
 
 restaurantList.push(new Restaurant(restaurantId++, "The Cheesecake Factory", "120 Brea Mall Way", "", "Brea", "CA", "92821", "(714) 255-0115", "thecheesecakefactory.com"))
 
-restaurantList.push(new Restaurant(restaurantId++, "Kula Revolving Sushi Bar", "1065 Brea Mall", "Ste 2050A", "Brea", "CA", 92821, "(714) 582-5898", "kurausa.com")) 
+restaurantList.push(new Restaurant(restaurantId++, "Kula Revolving Sushi Bar", "1065 Brea Mall", "Ste 2050A", "Brea", "CA", 92821, "(714) 582-5898", "kurausa.com"))
+
+restaurantList.push(new Restaurant(restaurantId++, "Charley's Steak", "1065 Brea Mall", "Ste 2050A", "Brea", "CA", 92821, "(714) 582-5898", "charleyssteak.com"))
 
 
 
