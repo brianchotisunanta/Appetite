e409665cd9662396408a8f6284f62b72a4ac0d6a
Brian Chotisunanta <bc.fsdev@gmail.com>
2017-11-22 20:13:54 -0800

    updating the restaurantsService.js; creating the POST GET PUT DELETE function() CRUD functionalities

--- public/services/restaurantsService.js
+++ public/services/restaurantsService.js
@@ -2,9 +2,27 @@ angular
   .module("restaurantApp")
   .service("restaurantService", function($http) {
 
-    //GET ALL MOVIES
+//GET ALL MOVIES
     this.getRestaurantList = function() {
       return $http.get("http://localhost:3000/restaurantList")
     }
 
+//POST
+    this.createRestaurant = function(restaurant) {
+      return $http.post("http://localhost:3000/restaurantList", {restaurant: restaurant})
+
+    }
+//GET
+    this.showRestaurant = function(restaurant) {
+      return $http.get("http://localhost:3000/restaurantList" + restaurant.id)
+    }
+
+//PUT
+    this.updateRestaurant = function(restaurant) {
+      return $http.put("http://localhost:3000/restaurantList" + restaurant.id {restaurant: restaurant})
+    }
+//DELETE
+    this.deleteRestaurant = function(restaurant) {
+      return $http.delete("http://localhost:3000/restaurantList" + restaurant.id)
+    }
   })
