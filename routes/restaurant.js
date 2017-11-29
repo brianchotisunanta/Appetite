df0354b32f3e6754ea657300e973ccfc41b6ad3e
Brian Chotisunanta <bc.fsdev@gmail.com>
2017-11-29 01:50:40 -0800

    updating and adding routes in node.js

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -3,9 +3,19 @@ var router = express.Router();
 
 var restaurantListController = require("../controllers/restaurant-controller.js")
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* GET www.something.com/restaurants */
+router.get('/', restaurantListController.index);
+
+// POST www.something.com/restaurants
+router.post('/', restaurantListController.create);
+
+//GET www.something.com/restaurants/:id
+router.get('/', restaurantListController.show);
+
+//PUT www.something.com/restaurants/:id
+router.put('/', restaurantListController.update);
+
+//DELETE www.something.com/restaurants/
+router.delete('/', restaurantListController.destroy);
 
 module.exports = router;
