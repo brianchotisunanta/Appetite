66e034b2f295ff7558413d81420f97468d56d8cc
Brian Chotisunanta <bc.fsdev@gmail.com>
2017-12-11 19:15:04 -0800

    added console.log inside of the CRUD functionality buttons to show which button is being pressed in the browser console.

--- public/controllers/restaurantsController.js
+++ public/controllers/restaurantsController.js
@@ -7,8 +7,10 @@ angular
       $scope.restaurantList = response.data.restaurantList
     })
 
-    var restaurantId = 11
+//Billy please look at restaurantID:
+    var restaurantId = 11;
 
+//Billy please look at my constructor:
     var Restaurant = function(restaurantId, restaurantName, restaurantAddress1, restaurantAddress2, restaurantCity, restaurantState, restaurantZipcode,restaurantPhoneNumber, restaurantWebsite) {
       this.id = restaurantId;
       this.name = restaurantName;
@@ -34,8 +36,9 @@ angular
 
 //************************ FUNCTIONS ************************
 
-// Submit Button:
+// SUBMIT Button: (THIS WORKS)
     $scope.submitRestaurant = function() {
+      console.log("Submit button test");
       restaurantsService.createRestaurant(new Restaurant(restaurantId++,
       $scope.name,
       $scope.address1,
@@ -54,42 +57,60 @@ angular
       $scope.address1 = ""
       $scope.address2 = ""
       $scope.city = ""
-      $scope
+      $scope.state = ""
       $scope.zipcode = ""
       $scope.phoneNumber = ""
       $scope.website = ""
 
-      //Warning for submitting blank input filed:
-          // $scope.missingName = function(name) {
-          //   if ($scope.name == ""){
-          //     missingName == true;
-          //   }
-          // }
+//Warning for submitting blank input filed:     (FIX THIS)
+      $scope.missingName = function(name) {
+        if ($scope.name == ""){
+          return missingName == true;
+        } else {
+          return name;
+        }
+      }
 
+      console.log($scope.restaurantList);
     }
 
     var currentRestaurantId = null;
     $scope.submitButton = true;       //shows Submit Button
     $scope.saveButton = false;        //hides Save Button
 
-// Update Button:
-    $scope.updateRestaurant = function(updatedRestaurant) {
-      currentRestaurantId = updatedRestaurant.id;
-      $scope.name = updatedRestaurant.name;
-      $scope.address1 = updatedRestaurant.address1;
-      $scope.address2 = updatedRestaurant.address2;
-      $scope.city = updatedRestaurant.city;
-      $scope.state = updatedRestaurant.state;
-      $scope.zipcode = updatedRestaurant.zipcode;
-      $scope.phoneNumber = updatedRestaurant.phoneNumber;
-      $scope.website = updatedRestaurant.website;
+//BIlly please look at my UPDATE Function; R.values are named based on my constructor value naming scheme:
+// UPDATE Button: (NOT WORKING)
+    $scope.updateRestaurant = function(R) {
+      console.log("Update button test");
+      currentRestaurantId = R.restaurantId;
+      $scope.name = R.name;
+      $scope.address1 = R.address1;
+      $scope.address2 = R.address2;
+      $scope.city = R.city;
+      $scope.state = R.state;
+      $scope.zipcode = R.zipcode;
+      $scope.phoneNumber = R.phoneNumber;
+      $scope.website = R.website;
+
+    // $scope.updateRestaurant = function(R) {
+    //   currentRestaurantId = R.restaurantId;
+    //   $scope.name = R.restaurantName;
+    //   $scope.address1 = R.restaurantAddress1;
+    //   $scope.address2 = R.restaurantAddress2;
+    //   $scope.city = R.restaurantCity;
+    //   $scope.state = R.restaurantState;
+    //   $scope.zipcode = R.restaurantZipcode;
+    //   $scope.phoneNumber = R.restaurantPhoneNumber;
+    //   $scope.website = R.restaurantWebsite;
 
       $scope.submitButton = false;    //hide
       $scope.saveButton = true;       //show
     }
 
-//Save Button:
+//Billy please look at my SAVE Function:
+//SAVE Button: (NOT WORKING)
     $scope.saveRestaurant = function() {
+      console.log("Save button test");
       var restaurant = new Restaurant(currentRestaurantId, $scope.name, $scope.address1, $scope.address2, $scope.city, $scope.state, $scope.zipcode, $scope.phoneNumber,$scope.website)
 
       restaurantsService.updateRestaurant(restaurant)
@@ -109,13 +130,19 @@ angular
       $scope.zipcode = "";
       $scope.phoneNumber = "";
       $scope.website = "";
+
+      $scope.showRestaurant = true;
+
     }
 
-//Delete Button:
+//DELETE Button: (THIS WORKS)
     $scope.deleteRestaurant = function(restaurant) {
+      console.log("Delete button test");
       restaurantsService.deleteRestaurant(restaurant)
         .then(function(response){
           $scope.restaurantList = response.data.restaurantList
         })
+
+        console.log($scope.restaurantList);
     }
   })
