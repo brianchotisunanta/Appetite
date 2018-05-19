5b9d985b931cdd7868b7014024eaa4b0c0efdf19
Brian Chotisunanta <bc.fsdev@gmail.com>
2018-05-19 04:15:06 -0700

    fixed the GET ONE RESTAURANT BY ITS ID functionality

--- public/services/restaurantsService.js
+++ public/services/restaurantsService.js
@@ -1,29 +1,57 @@
 angular
-  .module("restaurantApp")
-  .service("restaurantsService", function($http) {
+.module("restaurantApp")
+.service("restaurantsService", function($http) {
 
-//GET ALL RESTAURANTS IN THE restaurantList []:
+// GET ALL RESTAURANTS IN THE restaurantList []:
     this.getRestaurantList = function() {
       return $http.get("http://localhost:3000/restaurantList")
     }
 
-//POST
+// POST:
     this.createRestaurant = function(restaurant) {
       return $http.post("http://localhost:3000/restaurantList", {restaurant: restaurant})
 
     }
 
-//GET
-    this.showRestaurant = function(restaurant) {
-      return $http.get("http://localhost:3000/restaurantList/" + restaurant.id)
+// GET ONE RESTAURANT BY ITS ID:
+    this.getRestaurantById = function(id, callback) {
+      if ( id == "" || id == undefined || id == null ) {
+        var restaurant = {
+          id: "",
+          name: "",
+          address1: "",
+          address2: "",
+          city: "",
+          state: "",
+          zipcode: "",
+          phoneNumber: "",
+          website: ""
+        }
+        callback(restaurant)
+      }
+      else {
+        $http.get("http://localhost:3000/restaurantList/" + id)
+        .then(function(response) {
+            console.log(response);
+            console.log(response.data.restaurant);
+            callback(response.data.restaurant)
+        },
+        function(error) {
+            console.log(error);
+        })
+      }
     }
+    // this.showRestaurant = function(restaurant) {
+    //   return $http.get("http://localhost:3000/restaurantList/" + restaurant.id)
+    // }
 
-//PUT
+// PUT:
     this.updateRestaurant = function(restaurant) {
+      console.log(restaurant);
       return $http.put("http://localhost:3000/restaurantList/" + restaurant.id, {restaurant: restaurant})
     }
 
-//DELETE
+// DELETE:
     this.deleteRestaurant = function(restaurant) {
       return $http.delete("http://localhost:3000/restaurantList/" + restaurant.id)
     }
