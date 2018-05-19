f760cb3a3159ab5face4440bdbe681cc541b8734
Brian Chotisunanta <bc.fsdev@gmail.com>
2018-05-19 04:13:08 -0700

    refractored all the  CRUD functionalities and now they are all working

--- public/controllers/restaurantListController.js
+++ public/controllers/restaurantListController.js
@@ -1,126 +1,61 @@
 angular
-  .module("restaurantApp")
-  .controller("restaurantListController", function($scope, restaurantsService) {
-
-    restaurantsService.getRestaurantList()
-    .then(function(response){
-      $scope.restaurantList = response.data.restaurantList
-    })
-
-    var restaurantId = 11
-
-    var Restaurant = function(restaurantId, restaurantName, restaurantAddress1, restaurantAddress2, restaurantCity, restaurantState, restaurantZipcode,restaurantPhoneNumber, restaurantWebsite) {
-      this.id = restaurantId;
-      this.name = restaurantName;
-      this.address1 = restaurantAddress1;
-      this.address2 = restaurantAddress2;
-      this.city = restaurantCity;
-      this.state = restaurantState;
-      this.zipcode = restaurantZipcode;
-      this.phoneNumber = restaurantPhoneNumber;
-      this.website = restaurantWebsite;
-    }
-
-    $scope.name = ""
-    $scope.address1 = ""
-    $scope.address2 = ""
-    $scope.city = ""
-    $scope.state = ""
-    $scope.zipcode = ""
-    $scope.phoneNumber = ""
-    $scope.website = ""
+    .module('restaurantApp')
+    .controller("restaurantListController", function($scope, $state, $stateParams, restaurantsService) {
 
 
+      // ********** GET ALL the restaurants from the restaurantList[]: **********
+      restaurantsService.getRestaurantList()
+          .then(function(response) {
+                $scope.restaurantList = response.data.restaurantList
+                console.log("getRestaurantList function:", response);
+          }, function(error) {
+                console.log(error);
+          })
 
-//************************ FUNCTIONS ************************
-
-// Submit Button:
-    $scope.submitRestaurant = function() {
-
-      //Warning for submitting blank input filed:
-          $scope.missingName = function() {
-            if ($scope.name == ""){
-              missingName == true;
-            }
-            else {
-              missingName == false;
-            }
-          }
-
-      restaurantsService.createRestaurant(new Restaurant(restaurantId++,
-      $scope.name,
-      $scope.address1,
-      $scope.address2,
-      $scope.city,
-      $scope.state,
-      $scope.zipcode,
-      $scope.phoneNumber,
-      $scope.website))
-      .then(function(response) {
-        $scope.restaurantList = response.data.restaurantList
-      })
-
-
-      $scope.name = ""
-      $scope.address1 = ""
-      $scope.address2 = ""
-      $scope.city = ""
-      $scope
-      $scope.zipcode = ""
-      $scope.phoneNumber = ""
-      $scope.website = ""
-
-
-    }
-
-    var currentRestaurantId = null;
-    $scope.submitButton = true;       //shows Submit Button
-    $scope.saveButton = false;        //hides Save Button
-
-// Update Button:
-    $scope.updateRestaurant = function(updatedRestaurant) {
-      currentRestaurantId = updatedRestaurant.restaurantId;
-      $scope.name = updatedRestaurant.restaurantName;
-      $scope.address1 = updatedRestaurant.restaurantAddress1;
-      $scope.address2 = updatedRestaurant.restaurantAddress2;
-      $scope.city = updatedRestaurant.restaurantCity;
-      $scope.state = updatedRestaurant.restaurantState;
-      $scope.zipcode = updatedRestaurant.restaurantZipcode;
-      $scope.phoneNumber = updatedRestaurant.restaurantPhoneNumber;
-      $scope.website = updatedRestaurant.restaurantWebsite;
-
-      $scope.submitButton = false;    //hide
-      $scope.saveButton = true;       //show
-    }
-
-//Save Button:
-    $scope.saveRestaurant = function() {
-      var restaurant = new Restaurant(restaurantId, $scope.restaurantName, $scope.restaurantAddress1, $scope.restaurantAddress2, $scope.restaurantCity, $scope.restaurantState, $scope.restaurantZipcode, $scope.restaurantPhoneNumber, $scope.restaurantWebsite)
-
-      restaurantsService.updateRestaurant(restaurant)
-      .then(function(response) {
-        $scope.restaurantList = response.data.restaurantList
-      })
-
-      $scope.submitButton = true;   //show
-      $scope.saveButton = false;    //hide
-
-      currentRestaurantId = null;
-      $scope.name = "";
-      $scope.address1 = "";
-      $scope.address2 = "";
-      $scope.city = "";
-      $scope.state = "";
-      $scope.zipcode = "";
-      $scope.phoneNumber = "";
-      $scope.website = "";
-    }
-
-//Delete Button:
-    $scope.deleteRestaurant = function(restaurant) {
-      restaurantsService.deleteRestaurant(restaurant)
-        .then(function(response){
-          $scope.restaurantList = response.data.restaurantList
+      // ********** POST A RESTAURANT: **********
+      $scope.submitRestaurant = function() {
+        restaurantsService.createRestaurant($scope.restaurant)
+        .then(function(response) {
+              console.log("Submit function:", response);
+              $state.go("restaurants")
+        }, function(error) {
+              console.log(error);
         })
-    }
-  })
+      }
+
+      // ********** GET EACH RESTAURANT BY ITS ID: **********
+      if ($stateParams.id == "" || $stateParams.id == undefined || $stateParams.id == null) {
+          $scope.submitButton = true;   // shows the Submit Button
+          restaurantsService.getRestaurantById($stateParams.id, function(restaurant) {
+              $scope.restaurant = restaurant
+        })
+      } else {
+          $scope.submitButton = false;    // hides the Submit submitButton
+          restaurantsService.getRestaurantById($stateParams.id, function(restaurant) {
+              $scope.restaurant = restaurant
+        })
+      }
+
+      // ********** PUT (UPDATE) EACH RESTAURANT BY ID: **********
+      $scope.updateRestaurant = function() {
+        restaurantsService.updateRestaurant($scope.restaurant)
+            .then(function(response) {
+                console.log("Update function:", response);
+                $state.go("restaurants")
+            },
+            function(error) {
+                  console.log(error);
+            })
+      }
+
+      // ********** DELETE EACH RESTAURANT: **********
+      $scope.deleteRestaurant = function(restaurant) {
+        restaurantsService.deleteRestaurant(restaurant)
+            .then(function(response) {
+                $scope.restaurantList = response.data.restaurantList
+                console.log("Delete function:", $scope.restaurantList);
+            }, function(error) {
+                  console.log(error);
+            })
+      }
+    })
